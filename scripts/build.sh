a212fcf09182b60a2f948a73f04eae5632c68978
Boris Cherny <boris@performancejs.com>
2018-08-12 22:39:10 -0700

    update npm description

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ git clone git@github.com:bcherny/undux.git
 cd undux
 yarn
 sed -i '' 's/undux/undux-fb/g' package.json
+sed -i '' 's/Dead simple state management for React/Facebook-specific wrapper for Undux/g' package.json
 rm README.md
 cp ../README.md README.md
 yarn publish
